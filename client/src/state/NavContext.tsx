41b9790f6be65097e80454287afba43154be0986
Daniel Cender <danielcender@gmail.com>
2023-05-01 02:18:59 -0500

    fixing some other issues

--- client/src/state/NavContext.tsx
+++ client/src/state/NavContext.tsx
@@ -1,10 +1,25 @@
-import { useContext, createContext, PropsWithChildren, useState, SetStateAction } from 'react'
+import {
+  useContext,
+  createContext,
+  PropsWithChildren,
+  useState,
+  SetStateAction,
+  Dispatch,
+} from 'react'
 
 export type AppPage = 'home' | 'signup' | 'login' | 'worldselect' | 'avatarselect' | 'worldscene'
+const APP_PAGE_OPTIONS: AppPage[] = [
+  'home',
+  'signup',
+  'login',
+  'worldselect',
+  'avatarselect',
+  'worldscene',
+]
 
 export type NavContextFields = {
   currentPage: AppPage
-  setCurrentPage: React.Dispatch<SetStateAction<AppPage>>
+  setCurrentPage: Dispatch<SetStateAction<AppPage>>
 }
 
 const defaultUserContext: NavContextFields = {
@@ -16,11 +31,13 @@ export const NavContext = createContext<NavContextFields>(defaultUserContext)
 
 export const NavContextProvider: React.FunctionComponent<PropsWithChildren> = ({ children }) => {
   // takes terminating route from the URL. Only supports one level deep, no nested routes
-  const startingPage = location.href.slice(location.href.lastIndexOf('/') + 1) ?? null
+  const urlPathEnd = location.href.slice(location.href.lastIndexOf('/') + 1) ?? null
 
-  const [currentPage, setCurrentPage] = useState<AppPage>(
-    (startingPage as AppPage) ?? defaultUserContext.currentPage
-  )
+  const defaultPage = APP_PAGE_OPTIONS.includes(urlPathEnd as AppPage)
+    ? (urlPathEnd as AppPage)
+    : defaultUserContext.currentPage
+
+  const [currentPage, setCurrentPage] = useState<AppPage>(defaultPage)
 
   return (
     <NavContext.Provider
