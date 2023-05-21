fbcafe8a493d1eb190b16cba1538541fc43606eb
Daniel Cender <danielcender@gmail.com>
2023-05-20 23:01:15 -0500

    init scene ui uplift in progress, ws message fixes

--- client/src/pages/WorldScene.tsx
+++ client/src/pages/WorldScene.tsx
@@ -9,12 +9,33 @@ const SceneContainer = styled('div')`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+  flex-direction: row;
   width: 100vw;
   position: relative;
   height: 100%;
   background-color: ${(props: any) => props.theme.palette.primary.main};
   border-radius: 25px;
+`
+
+const SceneCommunicationBlock = styled('div')`
+  height: 100%;
+  padding-left: 30px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  flex-direction: column;
+  /* flex: 1; */
+`
+
+const SceneWhiteboardBlock = styled('div')`
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  /* flex: 2; */
 `
 
 const WorldScene = () => {
@@ -23,11 +44,13 @@ const WorldScene = () => {
       <WebSocketContextProvider>
         <SceneWrapper>
           <SceneContainer>
-            <div style={{ height: 150, width: 400, border: '1px solid red' }}>
-              Voice Chat Indicators
-            </div>
-            <ChatMessageBox />
-            {/* <ChatBox /> */}
+            <SceneCommunicationBlock>
+              <ChatMessageBox />
+            </SceneCommunicationBlock>
+            <SceneWhiteboardBlock>
+              <div style={{ border: '1px solid red', flex: 1 }}>Voice Chat Indicators</div>
+              <div style={{ border: '1px solid red', flex: 6 }}>Whiteboard will go here</div>
+            </SceneWhiteboardBlock>
           </SceneContainer>
         </SceneWrapper>
       </WebSocketContextProvider>
